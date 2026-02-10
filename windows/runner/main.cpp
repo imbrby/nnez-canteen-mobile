ff9e2fb6ef0b3cdb9e91e177360c33e4164a92a0
NotKnowbrother <3115920195@qq.com>
2026-02-10 14:50:14 +0800

    fix: adjust windows app size/name/icon and package workflow

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -26,8 +26,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
 
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
-  Win32Window::Size size(420, 900);
-  if (!window.Create(L"mobile_app", origin, size)) {
+  Win32Window::Size size(540, 960);
+  if (!window.Create(L"一粟", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
